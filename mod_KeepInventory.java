ad9c11169a1274501214f8bf20d616e7152606b9
Farn <93525542+FarnGitHub@users.noreply.github.com>
2025-08-24 18:08:07 +0700

    Update Mod v2

--- mod_KeepInventory.java
+++ mod_KeepInventory.java
@@ -5,10 +5,23 @@ import net.minecraft.client.Minecraft;
 public class mod_KeepInventory extends BaseMod {
 
     public mod_KeepInventory() {
-        PlayerAPI.RegisterPlayerBase(PlayerKeepInventory.class);
+        if(doesClassExist("mod_SinglePlayerCommands")) {
+            System.out.println("KeepInventoryMod: SPC Has been found this mod is disabled now");
+        } else {
+            PlayerAPI.RegisterPlayerBase(PlayerKeepInventory.class);
+        }
     }
 
     public String Version() {
-        return "yes";
+        return "2.0";
+    }
+
+    public static boolean doesClassExist(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 }
