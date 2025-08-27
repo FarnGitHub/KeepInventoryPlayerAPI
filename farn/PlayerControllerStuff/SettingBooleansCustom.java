85e3b0d8a803817c092044db4209336c82c24f84
Farn <93525542+FarnGitHub@users.noreply.github.com>
2025-08-27 18:05:31 +0700

    Fixed compatibility with older GUIAPI

--- farn/PlayerControllerStuff/SettingBooleansCustom.java
+++ farn/PlayerControllerStuff/SettingBooleansCustom.java
@@ -3,26 +3,22 @@ package farn.PlayerControllerStuff;
 import net.minecraft.src.*;
 
 public class SettingBooleansCustom extends SettingBoolean {
+    //instead of super class's backEndName we used our own name for compatibilitty with older GuiAPI
+    private String nameFarn = "nan";
 
     public SettingBooleansCustom(String name, Boolean def) {
         super(name, def);
+        this.nameFarn = name;
     }
 
     public void set(Boolean v, String context) {
         this.values.put(context, v);
-        ConfigFarn.instance.nameToBool.replace(this.backendName, v);
+        ConfigFarn.instance.nameToBool.replace(this.nameFarn, v);
         ConfigFarn.instance.saveOptions();
-
-        if(this.displayWidget != null) {
-            this.displayWidget.update();
-        }
-
     }
 
     public Boolean get(String context) {
-        return ConfigFarn.instance.nameToBool.getOrDefault(this.backendName, false);
+        return ConfigFarn.instance.nameToBool.getOrDefault(this.nameFarn, false);
     }
-
-
 
 }
